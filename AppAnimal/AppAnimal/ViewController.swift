8d4f5831df7d07fc4cb2dbf1e10e110ed458b307
Renan Junior <renan2junior@gmail.com>
2015-08-10 17:11:15 -0300

    Incluindo a biblioteca Alamofire para interação com serviço rest.

--- AppAnimal/AppAnimal/ViewController.swift
+++ AppAnimal/AppAnimal/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
+import Alamofire
+
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pegaDados();
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    func pegaDados(){
+        
+        Alamofire.request(.GET, "https://itunes.apple.com/search?term=angry&country=br&entity=software&lang=pt_br", parameters: ["kind": "software"])
+            .response { request, response, data, error in
+                println(request)
+                println(response)
+                println(error)
+        }
+        
+        
+        
     }
 
 

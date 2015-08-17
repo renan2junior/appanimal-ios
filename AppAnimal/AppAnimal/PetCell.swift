af9b442a3b76affe0b76e5357571c5e38830cf72
Renan Junior <renan2junior@gmail.com>
2015-08-17 16:45:42 -0300

    Incluindo arquivos faltantes.

---
+++ AppAnimal/AppAnimal/PetCell.swift
@@ -0,0 +1,26 @@
+//
+//  PetCell.swift
+//  AppAnimal
+//
+//  Created by renanjunior on 14/08/15.
+//  Copyright (c) 2015 renanjunior. All rights reserved.
+//
+
+import UIKit
+
+
+class PetCell: UITableViewCell{
+    
+   
+    @IBOutlet weak var nome : UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    
+}
